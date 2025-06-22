d40eae9507d963135d2f633e3f6f28f97843d1d3
ModistAndrew <zhujinxuan2005@163.com>
2025-06-22 15:36:44 +0800

    refactor project structure

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use indicatif::ProgressBar;
 use std::{fs::File, process::exit};
 
 fn main() {
-    let path = std::path::Path::new("output/book1/image1.jpg");
+    let path = std::path::Path::new("output/book1/image1.png");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
@@ -22,7 +22,6 @@ fn main() {
     for j in (0..height).rev() {
         for i in 0..width {
             let pixel = img.get_pixel_mut(i, j);
-
             let r: f64 = (i as f64) / ((width - 1) as f64) * 255.999;
             let g: f64 = (j as f64) / ((height - 1) as f64) * 255.999;
             let b: f64 = 0.25 * 255.999;
@@ -33,15 +32,8 @@ fn main() {
     progress.finish();
 
     println!(
-        "Ouput image as \"{}\"",
+        "Output image as \"{}\"",
         style(path.to_str().unwrap()).yellow()
     );
-    let output_image = image::DynamicImage::ImageRgb8(img);
-    let mut output_file = File::create(path).unwrap();
-    match output_image.write_to(&mut output_file, image::ImageOutputFormat::Jpeg(quality)) {
-        Ok(_) => {}
-        Err(_) => println!("{}", style("Outputting image fails.").red()),
-    }
-
-    exit(0);
+    img.save(path).expect("Cannot save the image to the file");
 }
