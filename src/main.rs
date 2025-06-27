990469ab50f291a37b27952b8486a8f49876bfbb
hezining <hezining@sjtu.edu.cn>
2025-06-28 00:27:38 +0800

    image4,忘记看warning

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ use std::io::Write;
 fn ray_color(r: Ray) -> Color {
     let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
     if t > 0.0 {
-        let N = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
-        return 0.5 * Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
-    let unit_direction = vec3::unit_vector(r.direction());
+    let unit_direction = unit_vector(r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
