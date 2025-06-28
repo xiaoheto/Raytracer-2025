48a58fa58907c6cc2cbabc3130cce81d2be62861
hezining <hezining@sjtu.edu.cn>
2025-06-28 19:21:26 +0800

    image18，没用到的函数加上#[allow(dead_code)]

--- src/easy_task/material.rs
+++ src/easy_task/material.rs
@@ -47,6 +47,7 @@ pub struct Metal {
 }
 
 impl Metal {
+    #[allow(dead_code)]
     pub fn new(albedo: Color, fuzz: f64) -> Self {
         Self { albedo, fuzz }
     }
@@ -74,6 +75,7 @@ pub struct Dielectric {
 }
 
 impl Dielectric {
+    #[allow(dead_code)]
     pub fn new(refraction_index: f64) -> Self {
         Self { refraction_index }
     }
