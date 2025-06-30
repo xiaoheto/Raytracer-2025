ef65dae7b5f732ef2327309e548e056558874459
hezining <hezining@sjtu.edu.cn>
2025-06-30 14:42:20 +0800

    book2/image6

--- src/easy_task/perlin.rs
+++ src/easy_task/perlin.rs
@@ -30,13 +30,43 @@ impl Default for Perlin {
 
 impl Perlin {
     pub fn noise(&self, p: Point3) -> f64 {
-        let i = ((4.0 * p.x()) as i32) & 255;
-        let j = ((4.0 * p.y()) as i32) & 255;
-        let k = ((4.0 * p.z()) as i32) & 255;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        self.randfloat[self.perm_x[i as usize] as usize
-            ^ self.perm_y[j as usize] as usize
-            ^ self.perm_z[k as usize] as usize]
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[(self.perm_x
+                        [((i as usize + di) as i32 & 255) as usize]
+                        ^ self.perm_y[((j as usize + dj) as i32 & 255) as usize]
+                        ^ self.perm_z[((k as usize + dk) as i32 & 255) as usize])
+                        as usize];
+                }
+            }
+        }
+        Self::trilinear_interp(c, u, v, w)
+    }
+
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
     fn perlin_generate_perm() -> Vec<i32> {
         let mut p = Vec::with_capacity(POINT_COUNT);
